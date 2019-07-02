4ac8d41c24c97d29d4296ad7b68ed3a7090eb046
Brandon Porter <brendlorend@gmail.com>
2019-07-01 17:26:42 -0700

    WIP: new styles merge

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import React from 'react';
 import './App.css';
-<<<<<<< HEAD
 import View from './components/Initiative'
-=======
-import Battlefield from "./components/Battlefield"
->>>>>>> origin/Jarred-Stanford
 
 function App() {
   return (
     <div className="App">
       <header>
-<<<<<<< HEAD
         <View/>
-=======
->>>>>>> origin/Jarred-Stanford
       </header>
-      <Battlefield />
     </div>
   );
 }
